8a06accd6e2f55e89d02db84c247ea4437eb87cb
jimrothstein <jimrothstein@gmail.com>
2026-02-01 23:04:12 -0800

    wip

--- 100_chat_github.R
+++ 100_chat_github.R
@@ -1,52 +1,23 @@
 
-# REF: https://drmowinckels.io/blog/2025/ollama/
+# simple chat
+
 
 library(ellmer)
-library(gander)
-library(chores)
-library(ensure)
+# ------------------------ WORKS
+
+# 2/26 WORKS!
+chat  <-   ellmer::chat_openrouter(model = "deepseek/deepseek-r1-0528:free")
+chat$chat("Tell me a joke about statisticians")
 
 
+# DOES NOT WORK   (ellmer error?)
+chat <- ellmer::chat_github()
 
-chores_chat  <-   ellmer::chat_github(model = "gpt-4o")
+# also no good 
+chat <- ellmer::chat_github(model = "gpt-4.1")
+chat$chat("What is R?")
 
-ensure_fn = "chat_github"
-ensure_args = list(model = "gpt-4o")
-.gander_chat = ellmer::chat_github(model = "gpt-4o")
-
-.gander_chat
-
-
-gander_chat("How many natural elements are there?")
-
-ls()
-
-?gander
-#----------------------- gemini
-Sys.getenv("GEMINI_API_KEY")
-ellmer::chat_google_gemini(api_key = Sys.getenv("GEMINI_API_KEY"))
-?chat_google_gemini
-
-
-
-
-?.Renviron
-
-Sys.getenv("GOOGLE_API_KEY")
-curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=GEMINI_API_KEY" \
-  -H 'Content-Type: application/json' \
-  -X POST \
-  -d '{
-    "contents": [
-      {
-        "parts": [
-          {
-            "text": "Explain how AI works in a few words"
-          }
-        ]
-      }
-    ] # end contents
-  }'
-
+#chat  <-   ellmer::chat_github(model = "gpt-4o")
+# chat  <-   ellmer::chat_openrouter(model = "gpt-4o")
 
 
--- aider.sh
+++ aider.sh
@@ -2,5 +2,10 @@
 # this model is headache
 #aider --model openrouter/kwaipilot/kat-coder-pro-v1
 
-# WORKS
-aider --model openrouter/mistralai/devstral-2512:free
+# WORKS  (use /ask)
+aider --model "openrouter/deepseek/deepseek-r1-0528:free"
+
+# NO
+# aider --model openrouter/xiaomi/mimo-v2-flash:free
+#aider --model openrouter/mistralai/devstral-2512:free
+
