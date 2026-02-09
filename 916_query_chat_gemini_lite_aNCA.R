5ecefff5dec9eb5daa69d623e4ff9780f1432c1e
jimrothstein <jimrothstein@gmail.com>
2026-02-08 21:57:37 -0800

    916 aNCA, translates English to SQL and runs

---
+++ 916_query_chat_gemini_lite_aNCA.R
@@ -0,0 +1,22 @@
+# 916_query_chat_gemini_lite_aNCA.R
+# Shiny App, uses querychat + csv data from aNCA
+# Allows natural lang questions of dataset (convers to SQL and runs)
+
+
+library(shiny)
+library(bslib)
+library(querychat)
+
+# load aNCA daa
+x <- read.csv("example-ADNCA.csv")
+
+chat <- ellmer::chat_google_gemini(
+  model = "gemini-2.5-flash-lite"
+)
+
+querychat_app(x,
+              greeting = "Translate English to SQL",
+              client = chat)
+
+
+
