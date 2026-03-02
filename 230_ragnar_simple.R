3e24d816c4332f10b02dfa18720cc98cda0a99ca
jimrothstein <jimrothstein@gmail.com>
2026-03-01 21:15:15 -0800

    wip

--- 230_ragnar_simple.R
+++ 230_ragnar_simple.R
@@ -1,7 +1,10 @@
+# STATUS
+#  errors when try to create Store;  wants to see function
+
 library(ragnar)
 library(envvar)
 url = "https://csgillespie.github.io/efficientR/"  
-md = read_as_markdown(url)
+md = ragnar::read_as_markdown(url)
 
 str(md)
 md |> typeof()
@@ -9,20 +12,20 @@ md |> class()    # S7
 
 # vectorize, 2 x 4, start, end, context, text
 chunks = markdown_chunk(md)
-chunks
-
-?ragnar_store_create
-?embed_google_gemini
-
-# get API key  (from .Renviron);  needs special key (says expired, not so)
-api_key = envvar::envvar_get("GEMINI_API_KEY_ragnar")
-api_key
 
 # Initialise a knowledge store with the Google Gemini embedding
 
 store = ragnar_store_create(
-  embed = embed_google_gemini()
+  embed = \(x) ragnar::embed_google_gemini(
+   x = chunks,
+   model = "gemini-embedding-001",
+   base_url = "https://generativelanguage.googleapis.com/v1beta",
+   api_key = api_key,
+   dims = NULL,
+   task_type = "RETRIEVAL_QUERY",
+   batch_size = 20L
+ )
 )
-
+# errros:   says object 'chunks' not found
 # Insert the Markdown chunks
 ragnar_store_insert(store, chunks)
